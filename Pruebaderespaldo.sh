04c3c9b61c3a91e681ed5f2701a8b942b66139e1
mayra290790 <mayra290790@users.noreply.github.com>
2025-07-02 15:10:40 -0300

    Agregando los archivos .

---
+++ Pruebaderespaldo.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/ksh
+
+# Variables donde estan los scripts
+script1="/home/wasadmin/backups/respaldo_cobisapp.sh"
+script2="/home/wasadmin/backups/backup_respaldo_profile.sh"
+script3="/home/wasadmin/backups/respaldo_profile_mq.sh"
+script4="/home/wasadmin/backups/respaldo_cobishome.sh"
+
+# Archivo de log
+log_file="/home/wasadmin/backups/registro_ejecucion.log"
+
+# Función para ejecutar y verificar
+ejecutar_script() {
+    local script_path=$1
+    local script_name=$(basename "$script_path")
+    
+    echo "Ejecutando $script_name..." | tee -a "$log_file"
+    
+    bash "$script_path" >> "$log_file" 2>&1
+    if [ $? -eq 0 ]; then
+        echo "$script_name se ejecutó con éxito." | tee -a "$log_file"
+    else
+        echo "Error al ejecutar $script_name." | tee -a "$log_file"
+    fi
+    echo "----------------------------------------" | tee -a "$log_file"
+}
+
+# Ejecutar los scripts
+ejecutar_script "$script1"
+ejecutar_script "$script2"
+ejecutar_script "$script3"
+ejecutar_script "$script3"
+
+echo "Proceso completo." | tee -a "$log_file"
